495a3ea988c1fccafa0bbc6d8a5050cb639a8393
iyonaga <yiyonaga@gmail.com>
2018-02-06 10:12:37 +0900

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  entry: {
+    main: './src/main.js'
+  },
+
+  output: {
+    filename: '[name].js',
+    path: path.join(__dirname, 'dist')
+  },
+
+  node: {
+    __dirname: false,
+    __filename: false
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+    ]
+  },
+
+  target: 'electron-main'
+}
