d2742d4e4f420364d3602fd9716ca41e8a9a7dd2
iyonaga <yiyonaga@gmail.com>
2018-03-21 01:14:38 +0900

    Rename file

--- src/renderer.js
+++ src/renderer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/_base.scss';
-import App from './containers/App';
+import App from './containers/app';
 
 ReactDOM.render(<App />, document.getElementById('app'));
 
